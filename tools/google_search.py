964fb8cfbe6c44bbedf639b727374b4775417d0a
malbiruk <2601074@gmail.com>
2024-07-16 15:12:16 +0400

    optimized summarization

--- tools/google_search.py
+++ tools/google_search.py
@@ -1,10 +1,12 @@
 import json
 from concurrent.futures import ThreadPoolExecutor, as_completed
+from datetime import datetime
 
 import streamlit as st
 from langchain.chains.summarize import load_summarize_chain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.tools import tool
+from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from selenium import webdriver
@@ -23,6 +25,7 @@ def create_browser(headless: bool = True, detached: bool = False) -> webdriver:
     (by default in headless mode)
     '''
     chrome_options = Options()
+    chrome_options.binary_location = "/usr/bin/chromium"
     if headless:
         chrome_options.add_argument('headless')
     # chrome_options.add_argument('start-maximized')
@@ -55,7 +58,7 @@ def fetch_page_content(link_idx: int, search_url: str) -> dict:
         pass
 
 
-def search_google(query: str) -> list[dict]:
+def search_google(query: str, n_results: int) -> list[dict]:
     '''
     uses headless browser to obtain all text content
     from the first Google page links
@@ -72,12 +75,12 @@ def search_google(query: str) -> list[dict]:
     search_url = driver.current_url
     links = driver.find_elements(By.XPATH, '//cite')
     n_true_links = len([link for link in links if link.text])
-    n_links = 5 if 5 <= n_true_links else n_true_links
+    n_links = n_results if n_results <= n_true_links else n_true_links
     driver.quit()
 
     results = []
 
-    with ThreadPoolExecutor(max_workers=5) as executor:
+    with ThreadPoolExecutor() as executor:
         future_to_link = {executor.submit(
             fetch_page_content, link_idx, search_url): link_idx
             for link_idx in range(n_links)}
@@ -127,18 +130,73 @@ def summarize(query, content, prompt=default_prompt) -> str:
     return summary_chain.run(input_documents=docs, query=query)
 
 
+def extract_relevant_information(query, content) -> str:
+    extract_prompt = """
+Extract chunks of relevant information for "{query}" from the text below:
+"{text}"
+RELEVANT INFORMATION:
+"""
+    prompt = PromptTemplate.from_template(extract_prompt)
+    extraction_chain = prompt | llm | StrOutputParser()
+    if len(content) < 100000 * 4:
+        return extraction_chain.invoke({"text": content, "query": query})
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=["\n\n", "\n"], chunk_size=30000, chunk_overlap=1000)
+    docs = text_splitter.split_text(content)
+
+    results = []
+
+    with ThreadPoolExecutor() as executor:
+        future_to_link = {executor.submit(
+            extraction_chain.invoke, {"text": doc, "query": query}): doc
+            for doc in docs}
+        for future in as_completed(future_to_link):
+            result = future.result()
+            results.append(result)
+
+    return extraction_chain.invoke({"text": '\n\n'.join(results), "query": query})
+
+
 @tool
-def web_search(query: str) -> str:
+def web_search(query: str, n_results: int) -> str:
     """
     fetches and returns the summarized text content \
-    from webpages of the first Google search page for a given query
-    along with the source URLs as json
-    """
-    full_pages = search_google(query)
-    if len('\n'.join([i['contents'] for i in full_pages])) > 14000 * 4:
-        return json.dumps([{'contents': summarize(query, res['contents']), 'link': res['link']}
-                           for res in full_pages if res['contents']], indent=2)
+    from top n_results webpages of the first Google search page \
+    for a given query along with the source URLs as json
 
+    10 pages load about 2 minutes (which is a lot), so for thorough \
+    study use n_results 5-10 and for quick searches 1-5
+    """
+    full_pages = search_google(query, n_results)
+
+    def process_page_extract(res, query):
+        if res['contents']:
+            return {'contents': extract_relevant_information(query, res['contents']),
+                    'link': res['link']}
+        return None
+
+    def process_page_summarize(res, query):
+        if res['contents']:
+            return {'contents': summarize(query, res['contents']),
+                    'link': res['link']}
+        return None
+
+    if llm.model_name == 'gpt-4o':
+        if len('\n'.join([i['contents'] for i in full_pages])) > 100000 * 4:
+            with ThreadPoolExecutor() as executor:
+                results = list(executor.map(
+                    lambda res: process_page_extract(res, query), full_pages))
+            results = [result for result in results if result is not None]
+            return json.dumps(results, indent=2)
+        return json.dumps(full_pages, indent=2)
+
+    if len('\n'.join([i['contents'] for i in full_pages])) > 14000 * 4:
+        with ThreadPoolExecutor() as executor:
+            results = list(executor.map(
+                lambda res: process_page_summarize(res, query), full_pages))
+        results = [result for result in results if result is not None]
+        return json.dumps(results, indent=2)
     return json.dumps(full_pages, indent=2)
 
 
@@ -153,6 +211,12 @@ def read_webpage(url: str, query: str) -> str:
     WebDriverWait(driver, 5).until(
         EC.presence_of_element_located((By.XPATH, '/html/body')))
     content = driver.find_element(By.XPATH, "/html/body").text
+
+    if llm.model_name == 'gpt-4o':
+        if len(content) > 100000 * 4:
+            return extract_relevant_information(query, content)
+        return content
+
     if len(content) > 14000 * 4:
         return summarize(query, content)
     return content
